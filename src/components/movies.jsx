ff35cdb4632a7b024401c20ff8ebe906935e924e
Kay Ha <kiwook.ha@gmail.com>
2019-01-19 23:07:17 +0900

    added movies count

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -12,33 +12,38 @@ class Movies extends Component {
   };
 
   render() {
+    let movieCount = this.state.movies.length;
+    if (movieCount === 0) return <p>There are no movies in the database.</p>;
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Stock</th>
-            <th>Rate</th>
-            <th />
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.movies.map(movie => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td>
-                <button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">
-                  Delete
-                </button>
-              </td>
+      <React.Fragment>
+        <p>Showing {movieCount} movies in the database.</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
     );
   }
 }
